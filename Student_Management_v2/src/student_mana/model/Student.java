c480df3a24e58b0300996bae63b64b1626155ba0
husky <hnduy9b2020@gmail.com>
2026-04-10 15:59:51 +0700

    Update model & File handle

--- Student_Management_v2/src/student_mana/model/Student.java
+++ Student_Management_v2/src/student_mana/model/Student.java
@@ -6,5 +6,73 @@ public class Student {
     private int age;
     private String address;
     private String email;
-    
+    private double gpa;
+
+    // Default constructor
+    public Student() {
+    }
+
+    public Student(String studentID, String name, int age, String address, String email, double gpa) {
+        this.studentID = studentID;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.email = email;
+        this.gpa = gpa;
+    }
+
+    // Gettters & setters for all
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [studentID=" + studentID + ", name=" + name + ", age=" + age + ", address=" + address
+                + ", email=" + email + ", gpa=" + gpa + "]";
+    }
 }
--- Student_Management_v2/src/student_mana/utils/FileHandler.java
+++ Student_Management_v2/src/student_mana/utils/FileHandler.java
@@ -1,5 +1,67 @@
 package student_mana.utils;
 
-public class FileHandler {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import student_mana.model.Student;
+
+public class FileHandler {
+    private String fileName = "students.txt";
+    public FileHandler() {
+    }
+
+    // Write data to file
+    public void saveToFile(List<Student> studentList) {
+        File file = new File(fileName);
+        File parentDir = file.getParentFile();
+        // Check if the parent folder(utils) does not have, then create new file
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs();
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            for (Student s : studentList) {
+                String line = s.getStudentID() + "|" +
+                        s.getAge() + "|" +
+                        s.getAddress() + "|" +
+                        s.getEmail() + "|" +
+                        s.getGpa();
+                bw.write(line);
+                bw.newLine();
+                // bw.close();
+            }
+            System.out.println("File is saved in " + fileName + " sucessfull!");
+        } catch (IOException e) {
+            System.out.println("Error while saving file: " + e.getMessage());
+        }
+    }
+
+    // Load data from file
+    public List<Student> loadFromFile() {
+        List<Student> sData = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split("|");
+                Student s = new Student(data[0], // student id
+                        data[1], // student name
+                        Integer.parseInt(data[2]), // Student age
+                        data[3], // address
+                        data[4], // email
+                        Double.parseDouble(data[5])); // gpa
+                sData.add(s);
+            }
+
+        } catch (IOException e) {
+            System.out.println("No file found!" + e.getMessage());
+        }
+        return sData;
+    }
+    // Export to csv file
 }
